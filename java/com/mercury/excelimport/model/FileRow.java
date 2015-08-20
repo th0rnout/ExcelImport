91a9d801dcb17b7758784f1f84fdd7058fc7ccde
Jakub Stepien <devfedake@gmail.com>
2015-08-20 20:42:55 +0200

    Added class that encapsulates a single row from Excel file

---
+++ java/com/mercury/excelimport/model/FileRow.java
@@ -0,0 +1,123 @@
+package com.mercury.excelimport.model;
+
+import java.util.Date;
+
+/**
+ * Created by Fedake on 2015-08-20.
+ */
+public class FileRow
+{
+    private String system;
+    private int request;
+    private String orderNumber;
+    private Date fromDate;
+    private Date toDate;
+    private float amount;
+    private String amountType;
+    private String amountPeriod;
+    private int authPercent;
+    private boolean active;
+
+    public FileRow()
+    {
+
+    }
+
+    public FileRow(String system, int request, String orderNumber, Date fromDate, Date toDate, float amount, String amountType, String amountPeriod, int authPercent, boolean active)
+    {
+        this.system = system;
+        this.request = request;
+        this.orderNumber = orderNumber;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.amount = amount;
+        this.amountType = amountType;
+        this.amountPeriod = amountPeriod;
+        this.authPercent = authPercent;
+        this.active = active;
+
+    }
+
+
+    // Getters and Setters
+
+    public String getSystem() {
+        return system;
+    }
+
+    public void setSystem(String system) {
+        this.system = system;
+    }
+
+    public int getRequest() {
+        return request;
+    }
+
+    public void setRequest(int request) {
+        this.request = request;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public Date getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(Date fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public Date getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(Date toDate) {
+        this.toDate = toDate;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getAmountType() {
+        return amountType;
+    }
+
+    public void setAmountType(String amountType) {
+        this.amountType = amountType;
+    }
+
+    public String getAmountPeriod() {
+        return amountPeriod;
+    }
+
+    public void setAmountPeriod(String amountPeriod) {
+        this.amountPeriod = amountPeriod;
+    }
+
+    public int getAuthPercent() {
+        return authPercent;
+    }
+
+    public void setAuthPercent(int authPercent) {
+        this.authPercent = authPercent;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
